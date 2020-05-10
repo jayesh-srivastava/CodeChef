6c087c05df595b515af756030b24b8fb122dddf3
Jayesh Srivastava <jayesh0200@gmail.com>
2020-05-10 21:43:16 +0530

    Update Solution.java

--- May Long Challenge 2020/Coronavirus Spread/Solution.java
+++ May Long Challenge 2020/Coronavirus Spread/Solution.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Covid19 {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
