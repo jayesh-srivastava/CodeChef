22c35a74d41d5838990fbb64378061ae339b82f5
Jayesh Srivastava <jayesh0200@gmail.com>
2020-06-09 17:04:47 +0530

    Update PRICECON.java

--- June Long Challenge 2020/Chef and Price Control/PRICECON.java
+++ June Long Challenge 2020/Chef and Price Control/PRICECON.java
@@ -1,1 +1,39 @@
+import java.util.*;
+class PRICECON
+{
+    public static int calculate(List<Integer> list,int n)
+    {
+        int sum=list.stream().mapToInt(i->i).sum();
+        for(int x=0;x<list.size();x++)
+        {
+            if(list.get(x)>n)
+            {
+                list.set(x,n);
+            }
+        }
+        int sum2=list.stream().mapToInt(i->i).sum();
+        return sum-sum2;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int tt=sc.nextInt();
+        int result[]=new int[tt];
+        for(int x=0;x<tt;x++)
+        {
+            int size=sc.nextInt();
+            int k=sc.nextInt();
+            List<Integer> list=new ArrayList<>();
+            for(int y=0;y<size;y++)
+            {
+                list.add(y,sc.nextInt());
+            }
+            result[x]=calculate(list,k);
+        }
+        for(int x:result)
+        {
+            System.out.println(x);
+        }
+    }
+}
 
