faba7dbfdddfcde2278c55a86213fa51b945edb8
Jayesh Srivastava <jayesh0200@gmail.com>
2020-08-08 23:49:07 +0530

    Update CHEFWARS.java

--- August Long Challenge 2020/CHEFWARS.java
+++ August Long Challenge 2020/CHEFWARS.java
@@ -1,1 +1,31 @@
+import java.util.*;
+import java.lang.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int tt=sc.nextInt();
+        while(tt-->0)
+        {
+            int d=sc.nextInt();
+            int c=sc.nextInt();
+            while(d>0 && c>0)
+            {
+                d=d-c;
+                c=c/2;
+            }
+            if(d>0)
+            {
+                System.out.println(0);
+            }
+            else
+            {
+                System.out.println(1);
+            }
+        }
+    }
+}
 
