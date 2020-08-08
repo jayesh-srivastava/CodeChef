8dc9ac3d8f1df378b99502e37da56a4198a2109c
Jayesh Srivastava <jayesh0200@gmail.com>
2020-08-09 01:46:34 +0530

    Update LINCHESS.java

--- August Long Challenge 2020/Chef and Linear Chess/LINCHESS.java
+++ August Long Challenge 2020/Chef and Linear Chess/LINCHESS.java
@@ -1,1 +1,33 @@
+import java.util.*;
+public class Linchess
+{
+    public static void  main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int tt=sc.nextInt();
+        int min=0;
+        while(tt-->0)
+        {
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int ar[]=new int[n];
+            for(int x=0;x<n;x++)
+            {
+                ar[x]=sc.nextInt();
+                if(k%ar[x]==0)
+                {
+                    if(ar[x]>min)
+                        min=ar[x];
+                }
+            }
+            if(min==0)
+            {
+                System.out.println(-1);
+            }
+            else
+                System.out.println(min);
+            min=0;
+        }
+    }
+}
 
