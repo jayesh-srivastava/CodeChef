fd7d0e265e47552633941944c4f7f6119446acb0
Jayesh Srivastava <jayesh0200@gmail.com>
2021-02-03 01:05:17 +0530

    Create UWCOI21A.java

---
+++ Hidden Numbers/UWCOI21A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class UWCOI21A
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int tt=sc.nextInt();
+        boolean flag=false;
+        int copy=0;
+        while(tt-->0)
+        {
+            int n=sc.nextInt();
+            System.out.println("1 "+n);
+        }
+    }
+}
